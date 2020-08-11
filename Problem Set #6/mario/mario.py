0b4351f117d6a8edc717293ea331c2a7c3199753
Damir Temir <temirdamir10@gmail.com>
2020-08-10 19:10:18 -0500

    Add files via upload

---
+++ Problem Set #6/mario/mario.py
@@ -0,0 +1,16 @@
+from cs50 import get_int
+
+height = get_int("Height: ")
+while height < 1 or height > 8:
+    height = get_int("Height: ")
+
+for i in range(height):
+    for j in range(height - i - 1):
+        print(" ", end="")
+    for b in range(i + 1):
+        print("#", end="")
+    print("  ", end="")
+    for b in range(i + 1):
+        print("#", end="")
+    print("")
+        
